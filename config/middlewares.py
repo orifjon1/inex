a2c87422aaec4fc5208e0ccf590e918c53e6e5a7
Orifjon <freejobmakery@gmail.com>
2023-06-08 18:36:59 +0500

    asgiii.py

--- config/middlewares.py
+++ config/middlewares.py
@@ -2,7 +2,6 @@ from urllib.parse import parse_qs
 
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import AnonymousUser
 from rest_framework_simplejwt.tokens import AccessToken, TokenError
 
 User = get_user_model()
@@ -10,10 +9,7 @@ User = get_user_model()
 
 @database_sync_to_async
 def get_user(user_id):
-    try:
-        return User.objects.get(id=user_id)
-    except User.DoesNotExist:
-        return AnonymousUser()
+    return User.objects.get(id=user_id)
 
 
 class WebSocketJWTAuthMiddleware:
