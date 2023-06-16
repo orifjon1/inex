3a430b518e60d8c393606ed8806006f0758a64e6
Orifjon <freejobmakery@gmail.com>
2023-06-16 19:22:34 +0500

    aaa

--- task/middlewares.py
+++ task/middlewares.py
@@ -2,63 +2,33 @@ from urllib.parse import parse_qs
 
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
-# from rest_framework_simplejwt.tokens import AccessToken, TokenError
+from django.contrib.auth.models import AnonymousUser
+from rest_framework_simplejwt.tokens import AccessToken, TokenError
 
-# User = get_user_model()
+User = get_user_model()
+
+
+@database_sync_to_async
+def get_user(user_id):
+    try:
+        return User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return AnonymousUser()
 
 
 class WebSocketJWTAuthMiddleware:
-    def __init__(self, inner):
-        self.inner = inner
+
+    def __init__(self, app):
+        self.app = app
 
     async def __call__(self, scope, receive, send):
-        from django.contrib.auth import get_user_model
-        User = get_user_model()
+        parsed_query_string = parse_qs(scope["query_string"])
+        token = parsed_query_string.get(b"token")[0].decode("utf-8")
 
         try:
-            token = JWTAuthentication().get_validated_token(scope["query_string"].decode("utf-8"))
-        except InvalidToken as e:
-            print(f"Invalid token: {e}")
-            return None
-        except TokenError as e:
-            print(f"Token error: {e}")
-            return None
+            access_token = AccessToken(token)
+            scope["user"] = await get_user(access_token["user_id"])
+        except TokenError:
+            scope["user"] = AnonymousUser()
 
-        user_id = token["user_id"]
-        user = await self.get_user(user_id)
-
-        if user is None:
-            print("User not found")
-            return None
-
-        scope['user'] = user
-
-        return await self.inner(scope, receive, send)
-
-    @database_sync_to_async
-    def get_user(self, user_id):
-        try:
-            return get_user_model().objects.get(id=user_id)
-        except get_user_model().DoesNotExist:
-            return None
-
-# @database_sync_to_async
-# def get_user(user_id):
-#     return User.objects.get(id=user_id)
-#
-#
-# class WebSocketJWTAuthMiddleware:
-#
-#     def __init__(self, app):
-#         self.app = app
-#
-#     async def __call__(self, scope, receive, send):
-#         parsed_query_string = parse_qs(scope["query_string"])
-#         token = parsed_query_string.get(b"token")[0].decode("utf-8")
-#
-#         access_token = AccessToken(token)
-#         scope["user"] = await get_user(access_token["user_id"])
-#
-#         return await self.app(scope, receive, send)
+        return await self.app(scope, receive, send)
